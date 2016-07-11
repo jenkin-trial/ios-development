23621493a6ad5b22b18f641225e6bbd915f919a3
pallavi <pallavi.snehal@accenture.com>
2016-07-11 13:38:28 +0530

    1st commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Snehal, Pallavi on 08/07/16.
+//  Copyright © 2016 Snehal, Pallavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+
+
+
+}
+
